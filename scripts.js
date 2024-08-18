692d25fcd8e82088a52c7d85ea37aa21339d241f
Pikakid98 <12392651+pikakid98@users.noreply.github.com>
2024-08-19 00:57:00 +0100

    ported the markdown to HTML and added footer + icons

---
+++ scripts.js
@@ -0,0 +1,4 @@
+        // Add current year to footer
+        const currentYear = new Date().getFullYear();
+        const footerText = `Pikakid98 Launcher © Pikakid98 2021 - ${currentYear} / Octocat © GitHub 2008 - ${currentYear} / Windows logo © Microsoft 1975 - ${currentYear}`;
+        document.getElementById('footer').innerText = footerText;
